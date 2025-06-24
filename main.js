253301aa091fc8d7f9d3251bba292c6cf25b603d
renegd7 <138507055+becaks1@users.noreply.github.com>
2025-06-24 07:56:46 -0300

    grg

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const numeroSenha = document.querySelector(".parametro-senha_texto");
 const botaoSenha = document.querySelector("parametros-senha_botao");
 
-let tamanhoSenha = 8;
+let tamanhoSenha = 6;
 numeroSenha.textContent = tamanhoSenha;
 
 console.log(botaoSenha);
 
 botaoSenha[0].onclick = diminuiTamanhoSenha = () => {
-    if(tamanhoSenha > 1){
-    
-       tamanhoSenha--;
-}
+    if (tamanhoSenha > 1) {
+
+        tamanhoSenha--;
     }
-    numeroSenha.textContent = tamanhoSenha;
-   
+}
+numeroSenha.textContent = tamanhoSenha;
+
 botaoSenha[1].onclick = aumentaTamanhoSenha = () => {
     if (tamanhoSenha < 20) {
 
         tamanhoSenha++;
-    
-    numeroSenha.textContent = tamanhoSenha;
+
+        numeroSenha.textContent = tamanhoSenha;
     }
 };
