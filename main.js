e04439c7581d5d8b825d19f4bf0cc092ccbfcdcd
renegd7 <138507055+becaks1@users.noreply.github.com>
2025-06-24 07:50:42 -0300

    b

---
+++ main.js
@@ -0,0 +1,11 @@
+const numeroSenha = document.querySelector(".parametro-senha_texto");
+const botaoSenha = document.querySelector("parametros-senha_botao");
+
+let tamanhoSenha = 8;
+numeroSenha.textContent = tamanhoSenha;
+
+console.log(botaoSenha);
+
+botaoSenha[0].onclick = diminuiTamanhoSenha = () => {
+    numeroSenha.textContent = tamanhoSenha;
+}
