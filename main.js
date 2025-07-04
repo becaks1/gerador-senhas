ce5e1064a59a91e62af38822857f9d06e6e9f9bb
renegd7 <138507055+becaks1@users.noreply.github.com>
2025-07-04 10:14:50 -0300

    tb

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-const numeroSenha = document.querySelector(".parametro-senha_texto");
-const botaoSenha = document.querySelector("parametros-senha_botao");
+const numeroSenha = document.querySelector(".parametros-senha__texto");
+const botaoSenha = document.querySelectorAll(".parametros-senha__botao");
+const campoSenha = document.querySelector("#campo-senha");
 
-let tamanhoSenha = 6;
+let tamanhoSenha = 4;
 numeroSenha.textContent = tamanhoSenha;
 
-console.log(botaoSenha);
+//console.log(botaoSenha);
 
 botaoSenha[0].onclick = diminuiTamanhoSenha = () => {
     if (tamanhoSenha > 1) {
-
         tamanhoSenha--;
     }
+    numeroSenha.textContent = tamanhoSenha;
+    gerarSenha();
 };
-numeroSenha.textContent = tamanhoSenha;
 
 botaoSenha[1].onclick = aumentaTamanhoSenha = () => {
     if (tamanhoSenha < 20) {
-
         tamanhoSenha++;
-
-        numeroSenha.textContent = tamanhoSenha;
     }
+    numeroSenha.textContent = tamanhoSenha;
+    gerarSenha();
 };
+
+const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+campoSenha.value = letrasMaiusculas;
+gerarSenha();
+
+function gerarSenha() {
+    let senhaGerada = "";
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.floor(Math.random() * letrasMaiusculas.length);
+        //console.log(letrasMaiusculas[numeroAleatorio]);
+        senhaGerada += letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senhaGerada;
+}
